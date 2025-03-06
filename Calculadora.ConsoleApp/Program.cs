55d5ade846fde46881ba7770b0d91639dcd49f6e
leandro jader <ldandro>
2025-03-06 17:08:56 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("calculadoa tabajara 2025");
+            Console.WriteLine("digite o primeiro numero");
+            string primeironumerostring  = Console.ReadLine();
+            int primeironumero = Convert.ToInt32(primeironumerostring);
+            Console.WriteLine("digite o segundo  numero");
+            string segundonumeroString = Console.ReadLine();
+            int SegundoNumero = Convert.ToInt32(primeironumerostring);
+
+            int resultado = primeironumero + SegundoNumero;
+
+            Console.WriteLine("o resultado e " + resultado);
+
+
+
+       
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
