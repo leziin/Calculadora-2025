0e93a5c05711ce802b337b3958949fa0e12f8485
leandro jader <ldandro>
2025-03-07 08:49:16 -0300

    implementando o titulo do projeto

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,19 +4,7 @@
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("calculadoa tabajara 2025");
-            Console.WriteLine("digite o primeiro numero");
-            string primeironumerostring  = Console.ReadLine();
-            int primeironumero = Convert.ToInt32(primeironumerostring);
-            Console.WriteLine("digite o segundo  numero");
-            string segundonumeroString = Console.ReadLine();
-            int SegundoNumero = Convert.ToInt32(primeironumerostring);
-
-            int resultado = primeironumero + SegundoNumero;
-
-            Console.WriteLine("o resultado e " + resultado);
-
+            Console.WriteLine("calculadora tabajara 2025");
 
 
        
