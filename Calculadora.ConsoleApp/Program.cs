7e4e9c5b1e1acd84c82e10e9581428957ddd4e67
leandro jader <ldandro>
2025-03-07 08:52:13 -0300

    implementando a conversao de string para int

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -5,8 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("calculadora tabajara 2025");
-
-
+            Console.WriteLine("digite o primeiro numero");
+            string PrimeiroNumero = Console.ReadLine();// aquui a informaçao esta sendo recolhida como string.
+            int Numero1 = Convert.ToInt32(PrimeiroNumero);//aqui esta sendo feita a conversao de string para int.
+           
        
             Console.ReadLine();
 
