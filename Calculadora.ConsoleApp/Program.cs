8a6d9db0682fc817b16f97ed9f4a6f3e83518f1f
leandro jader <ldandro>
2025-03-07 08:56:37 -0300

    implementando a conversão da segunda variavel e o resultado da soma de dois numeros

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -8,8 +8,13 @@
             Console.WriteLine("digite o primeiro numero");
             string PrimeiroNumero = Console.ReadLine();// aquui a informaçao esta sendo recolhida como string.
             int Numero1 = Convert.ToInt32(PrimeiroNumero);//aqui esta sendo feita a conversao de string para int.
-           
-       
+            Console.WriteLine("digite o segundo numero");
+            string SegundoNumero = Console.ReadLine();
+            int Numero2 = Convert.ToInt32(SegundoNumero);
+
+            int resultado = Numero1 + Numero2;
+            Console.WriteLine("o resultado e "+resultado);
+
             Console.ReadLine();
 
 
