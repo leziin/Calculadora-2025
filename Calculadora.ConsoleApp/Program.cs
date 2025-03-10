aed4ae971de5fa3ac5b198f89a0817ab188cee89
leandro jader <ldandro>
2025-03-10 14:29:48 -0300

    implementada a estrutura de repetição

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -3,53 +3,79 @@
     internal class Program
     {
         static void Main(string[] args)
-        
-       
+
+
         {
-            Console.WriteLine("calculadora tabajara 2025");
-            Console.WriteLine("===========================");
-            Console.WriteLine("digite 1 para somar ");
-            Console.WriteLine("digite 2 para subtrair ");
-            Console.WriteLine("digite 5 para sair ");
-            Console.WriteLine("===========================");
-            Console.WriteLine("digite uma opcao");
+           
+          
+            while (true) {
+                Console.Clear();
+                Console.WriteLine("calculadora tabajara 2025");
+                Console.WriteLine("===========================");
+                Console.WriteLine("digite 1 para somar ");
+                Console.WriteLine("digite 2 para subtrair ");
+                Console.WriteLine("digite 5 para sair ");
+                Console.WriteLine("===========================");
+                Console.WriteLine("digite uma opcao");
 
-            string opcao = Console.ReadLine().ToUpper();// se o usuario digitar s minusculo ele converte para maiusculo.
-       
-
-            if (opcao == "S")
-            {
-                return;
-            }
+                string opcao = Console.ReadLine().ToUpper(); // se o usuario digitar s minusculo ele converte para maiusculo. 
 
 
-            Console.WriteLine("digite o primeiro numero");
-            string PrimeiroNumero = Console.ReadLine();// aqui a informaçao esta sendo recolhida como string.
 
 
-            int Numero1 = Convert.ToInt32(PrimeiroNumero);//aqui esta sendo feita a conversao de string para int.
-            Console.WriteLine("digite o segundo numero");
+                if (opcao == "S")
+                {
+                    break;
+                  
+                }
 
 
-            string SegundoNumero = Console.ReadLine();// aqui a informaçao esta sendo recolhida como string.
-            int Numero2 = Convert.ToInt32(SegundoNumero);//aqui esta sendo feita a conversao de string para int.
+                Console.WriteLine("digite o primeiro numero");
+                string PrimeiroNumero = Console.ReadLine();// aqui a informaçao esta sendo recolhida como string.
 
-            int resultado = 0;
 
-            if (opcao=="1")
-            {
-                resultado = Numero1 + Numero2;
-            }
+                int Numero1 = Convert.ToInt32(PrimeiroNumero);//aqui esta sendo feita a conversao de string para int.
+                Console.WriteLine("digite o segundo numero");
 
-            else 
-            {
-                resultado = Numero1 - Numero2;
-            }
 
-            Console.WriteLine("o resultado e "+resultado);
+                string SegundoNumero = Console.ReadLine();// aqui a informaçao esta sendo recolhida como string.
+                int Numero2 = Convert.ToInt32(SegundoNumero);//aqui esta sendo feita a conversao de string para int.
+
+                int resultado = 0;
+
+                if (opcao == "1")
+                {
+                    resultado = Numero1 + Numero2;
+                }
+
+                else
+                {
+                    resultado = Numero1 - Numero2;
+                }
+            Console.WriteLine("o resultado e "+ resultado);
             Console.ReadLine();
+
+
+                Console.WriteLine("voce deseja continuar  digite s/n");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "s")
+                {
+                    break;
+                }
+
+                Console.ReadLine();
+            }
+
+
+
+
+
+
+
 
 
         }
     }
 }
+   
